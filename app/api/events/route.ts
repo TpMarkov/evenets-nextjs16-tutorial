775d92cbec7ebd4a2283e996f38a493f4a2011e7
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-11-09 09:06:47 +0200

    add create event

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,3 +1,28 @@
-export async function POST() {
+import {NextRequest, NextResponse} from "next/server";
+import connectDB from "@/lib/mongodb";
+import Event from "@/database/event.model"
+
+export async function POST(req: NextRequest) {
+
+    try {
+        await connectDB()
+        const formData = await req.formData()
+
+        let event
+
+        try {
+            event = Object.fromEntries(formData.entries())
+        } catch (err) {
+            return NextResponse.json({message: err instanceof Error ? err.message : "Unknown"}, {status: 500})
+        }
+
+        const createdEvent = await Event.create(event)
+
+        return NextResponse.json({message: "Event created", createdEvent}, {status: 200})
+
+    } catch (err) {
+        return NextResponse.json({message: "Failed to create Event"}, {status: 500})
+    }
+
 
 }
